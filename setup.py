ce4e66dd94946d6445822988b84fbefb7d223d83
SajibMamun <sajibmamun138@gmail.com>
2025-06-16 18:51:52 +0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sajib Bin Mamun',
+    author_email= 'sajibmamun138@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
